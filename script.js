a6517db7a88e868784abaabff4111fa69bdd40d9
kimya-h <kimya.mailbox@gmail.com>
2024-03-14 14:31:15 +0330

    adding javascript and finish

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
-const slidesLenght = document.querySelectorAll('.div').length
+const slidesLenght = slideRight.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
-slideLeft.computedStyleMap.top = ~$
+slideLeft.style.top = `-${(slidesLenght - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up')
+    activeSlideIndex++
+    if(activeSlideIndex > slidesLenght - 1){
+        activeSlideIndex = 0
+    } else if(direction === 'down'){
+        activeSlideIndex--
+    if(activeSlideIndex < 0){
+        activeSlideIndex = slidesLenght - 1
+    }
+    }
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
